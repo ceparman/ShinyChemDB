48c7db748b14638555b42fa6bd6b85d09e205178
ceparman <info@ngsanalytics.com>
2021-05-20 19:20:42 -0400

    first pass mongo db seaching working

---
+++ dev/dbexample.py
@@ -0,0 +1,13 @@
+%matplotlib inline
+%pylab inline
+from IPython.display import Image
+from IPython.html.widgets import FloatProgress
+from IPython.display import display
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.interpolate import interp1d
+import random
+import time
+import sys
+import os
+from itertools import groupby
